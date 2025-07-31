c48dddce81853ae716de8494a8a66d8919cc906f
Ben Key <54153434+mellophone@users.noreply.github.com>
2025-07-30 21:49:56 -0500

    Add socket connection listeners

--- server/src/utils/GameServer.ts
+++ server/src/utils/GameServer.ts
@@ -1,9 +1,26 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 export default class GameServer {
   private server: Server;
 
   constructor(port: number) {
     this.server = new Server(port);
+
+    this.server.on("connect", this.onConnect);
+    console.log(`Server has started on port ${port}`);
+
+    process.on("SIGINT", () => {
+      this.server.close();
+      console.log("Server has closed");
+    });
   }
+
+  private onConnect = (socket: Socket) => {
+    console.log(`Socket with session id: ${socket.id} has connected`);
+    socket.on("disconnect", () => this.onDisconnect(socket));
+  };
+
+  private onDisconnect = (socket: Socket) => {
+    console.log(`Socket with session id: ${socket.id} has diconnected`);
+  };
 }
